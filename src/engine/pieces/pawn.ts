e6a9c2aa552c91ca774784b4217d0df51ea72699
Eduard Spirache <eduard.spirache@softwire.com>
2023-06-28 16:46:33 +0300

    made pawns move

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,13 +1,37 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
+    private moved: boolean;
+
     public constructor(player: Player) {
         super(player);
+        this.moved = false;
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves: Array<Square> = [];
+        let square: Square = board.findPiece(this);
+
+        if (this.player == Player.WHITE) {
+            if (!this.moved) {
+                availableMoves.push(new Square(square.row + 2, square. col));
+            }
+            availableMoves.push(new Square(square.row + 1, square. col));
+        } else {
+            if (!this.moved) {
+                availableMoves.push(new Square(square.row - 2, square. col));
+            }
+            availableMoves.push(new Square(square.row - 1, square. col));
+        }
+
+        return availableMoves;
+    }
+
+    public moveTo(board: Board, newSquare: Square) {
+        super.moveTo(board, newSquare);
+        this.moved = true;
     }
 }
