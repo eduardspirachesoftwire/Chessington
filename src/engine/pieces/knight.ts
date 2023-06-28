c4cc7af3a5942e797b32aaca608ffd552b1cc4d5
Eduard Spirache <eduard.spirache@softwire.com>
2023-06-28 17:52:32 +0300

    made knight move

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,46 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves: Array<Square> = [];
+        let square: Square = board.findPiece(this);
+
+        if (square.row + 2 < 8) {
+            if (square.col - 1 >= 0) {
+                availableMoves.push(new Square(square.row + 2, square.col - 1));
+            }
+            if (square.col + 1 < 8) {
+                availableMoves.push(new Square(square.row + 2, square.col + 1));
+            }
+        }
+
+        if (square.row - 2 >= 0) {
+            if (square.col - 1 >= 0) {
+                availableMoves.push(new Square(square.row - 2, square.col - 1));
+            }
+            if (square.col + 1 < 8) {
+                availableMoves.push(new Square(square.row - 2, square.col + 1));
+            }
+        }
+
+        if (square.col - 2 >= 0) {
+            if (square.row - 1 >= 0) {
+                availableMoves.push(new Square(square.row - 1, square.col - 2));
+            }
+            if (square.row + 1 < 8) {
+                availableMoves.push(new Square(square.row + 1, square.col - 2));
+            }
+        }
+
+        if (square.col + 2 < 8) {
+            if (square.row - 1 >= 0) {
+                availableMoves.push(new Square(square.row - 1, square.col + 2));
+            }
+            if (square.row + 1 < 8) {
+                availableMoves.push(new Square(square.row + 1, square.col + 2));
+            }
+        }
+
+
+        return availableMoves;
     }
 }
