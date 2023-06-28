f69d3ce18bbbe2053a36af70c4009e6862e537f6
Eduard Spirache <eduard.spirache@softwire.com>
2023-06-28 17:41:18 +0300

    made bishop move

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,41 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves: Array<Square> = [];
+        let square: Square = board.findPiece(this);
+
+        // Forward right
+        for (let i = square.row + 1, j = square.col + 1; i < 8 && j < 8; i++, j++) {
+            availableMoves.push(new Square(i, j));
+            if (board.getPiece(new Square(i, j)) !== undefined) {
+                break;
+            }
+        }
+
+        // Forward left
+        for (let i = square.row + 1, j = square.col - 1; i < 8 && j >= 0; i++, j--) {
+            availableMoves.push(new Square(i, j));
+            if (board.getPiece(new Square(i, j)) !== undefined) {
+                break;
+            }
+        }
+
+        // Backwards left
+        for (let i = square.row - 1, j = square.col - 1; i >= 0 && j >= 0; i--, j--) {
+            availableMoves.push(new Square(i, j));
+            if (board.getPiece(new Square(i, j)) !== undefined) {
+                break;
+            }
+        }
+
+        // Backwards right
+        for (let i = square.row - 1, j = square.col + 1; i >= 0 && j < 8; i--, j++) {
+            availableMoves.push(new Square(i, j));
+            if (board.getPiece(new Square(i, j)) !== undefined) {
+                break;
+            }
+        }
+
+        return availableMoves;
     }
 }
