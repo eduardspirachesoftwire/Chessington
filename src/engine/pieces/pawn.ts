174af3ef77a33feb7813be29cc5c19c3d8096fee
Eduard Spirache <eduard.spirache@softwire.com>
2023-06-29 10:49:40 +0300

    fixed pawn

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from "../square";
+import player from "../player";
 
 export default class Pawn extends Piece {
     private moved: boolean;
@@ -11,18 +12,43 @@ export default class Pawn extends Piece {
         this.moved = false;
     }
 
+    private checkForBlack(board: Board) {
+        let square: Square = board.findPiece(this);
+        if (this.player !== Player.BLACK) {
+            return false;
+        }
+        if (square.row - 1 < 0) {
+            return false;
+        }
+        if (board.getPiece(new Square(square.row - 1, square.col)) !== undefined) {
+            return false;
+        }
+        return true;
+    }
+
+    private checkForWhite(board: Board) {
+        let square: Square = board.findPiece(this);
+        if (this.player !== Player.WHITE) {
+            return false;
+        }
+        if (square.row + 1 > 7) {
+            return false;
+        }
+        if (board.getPiece(new Square(square.row + 1, square.col)) !== undefined) {
+            return false;
+        }
+        return true;
+    }
     public getAvailableMoves(board: Board) {
         let availableMoves: Array<Square> = [];
         let square: Square = board.findPiece(this);
 
-        if (this.player == Player.WHITE &&
-                board.getPiece(new Square(square.row + 1, square.col)) === undefined) {
+        if (this.checkForWhite(board)) {
             if (!this.moved && board.getPiece(new Square(square.row + 2, square.col)) === undefined) {
                 availableMoves.push(new Square(square.row + 2, square.col));
             }
             availableMoves.push(new Square(square.row + 1, square.col));
-        } else if (this.player == Player.BLACK &&
-                board.getPiece(new Square(square.row - 1, square.col)) === undefined) {
+        } else if (this.checkForBlack(board)) {
             if (!this.moved && board.getPiece(new Square(square.row - 2, square.col)) === undefined) {
                 availableMoves.push(new Square(square.row - 2, square.col));
             }
