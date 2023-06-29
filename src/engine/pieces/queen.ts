f8c4c202c33f90bf796ae4b46a2a3539af54bbbd
Eduard Spirache <eduard.spirache@softwire.com>
2023-06-29 12:13:01 +0300

    updated queen

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from "../square";
+import King from "./king";
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -72,6 +73,13 @@ export default class Queen extends Piece {
             }
         }
 
-        return availableMoves;
+        let updatedMoves: Array<Square> = [];
+        for (let move of availableMoves) {
+            if (board.getPiece(move) instanceof King || board.getPiece(move)?.player === this.player) {
+                continue;
+            }
+            updatedMoves.push(move);
+        }
+        return updatedMoves;
     }
 }
